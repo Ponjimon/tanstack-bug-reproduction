cf17d8840b3a819e6f81eb5e02c335a263b94567
Simon <770632+Ponjimon@users.noreply.github.com>
2024-09-30 15:42:22 +0000

    refactor: export client to remove build warning

--- app/client.tsx
+++ app/client.tsx
@@ -1,9 +1,14 @@
-import { StartClient } from '@tanstack/start'
 /// <reference types="vinxi/types/client" />
+import { StartClient } from '@tanstack/start'
 import { hydrateRoot } from 'react-dom/client'
+
 import { createRouter } from './router'
 
-const router = createRouter()
+void client()
 
-// biome-ignore lint/style/noNonNullAssertion: is ok to assert here
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+export default function client() {
+  const router = createRouter()
+
+  // biome-ignore lint/style/noNonNullAssertion: is ok to assert here
+  hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+}
