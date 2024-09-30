8eb8f2556cfff9cdd920652c21b7964e6b842d3d
Simon <770632+Ponjimon@users.noreply.github.com>
2024-09-30 18:16:32 +0000

    chore: add dep

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from '@tanstack/start/config'
 import type { App } from 'vinxi'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 const tanstackApp = defineConfig({
+  vite: {
+    plugins: () => [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+    ],
+  },
   server: {
     routeRules: {
       '/**': {},
