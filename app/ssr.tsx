1bb9eccd675e9bca7af75263c8115f07ce78a344
Simon <770632+Ponjimon@users.noreply.github.com>
2024-09-30 15:48:17 +0000

    refactor: move reference to top

--- app/ssr.tsx
+++ app/ssr.tsx
@@ -1,5 +1,5 @@
-import { getRouterManifest } from '@tanstack/start/router-manifest'
 /// <reference types="vinxi/types/server" />
+import { getRouterManifest } from '@tanstack/start/router-manifest'
 import { createStartHandler, defaultStreamHandler } from '@tanstack/start/server'
 
 import { createRouter } from './router'
